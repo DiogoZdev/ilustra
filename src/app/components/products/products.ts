9ef97e90eaebe2bda5e6b721ce8d6c15def028b8
Diogo <diogozang2@gmail.com>
2022-08-13 14:13:19 -0300

    projects collection correction

--- src/app/components/products/products.ts
+++ src/app/components/products/products.ts
@@ -1,31 +1,31 @@
 export const productsList = [
     {
       title: "Capa Notebook",
-      collection: "JUBARTE",
+      collection: "Jubarte",
       url: "https://querodobra.com.br/produto/capa-notebook-jubah/?ref=3527",
       imageUrl: "https://raw.githubusercontent.com/andressadesign/files/main/images-produtos/capa-note-baleia.png"
     },
     {
       title: "Carteira",
-      collection: "JUBARTE",
+      collection: "Jubarte",
       url: "https://querodobra.com.br/produto/nova-jubah/?ref=3527",
       imageUrl: "https://raw.githubusercontent.com/andressadesign/files/main/images-produtos/carteira-baleia.png"
     },
     {
       title: "Carteira",
-      collection: "GATO MAGICO",
+      collection: "Gato Mágico",
       url: "https://querodobra.com.br/produto/nova-carteira-classica-gato-magico/?ref=3527",
       imageUrl: "https://raw.githubusercontent.com/andressadesign/files/main/images-produtos/carteira-gato.png"
     },
     {
       title: "Porta Moedas",
-      collection: "GATO MAGICO",
+      collection: "Gato Mágico",
       url: "https://querodobra.com.br/produto/porta-moedas-gato-magico/?ref=3527",
       imageUrl: "https://raw.githubusercontent.com/andressadesign/files/main/images-produtos/porta-moedas-gato.png"
     },
     {
       title: "Necessaire",
-      collection: "GATO MAGICO",
+      collection: "Gato Mágico",
       url: "https://querodobra.com.br/produto/necessaire-gato-magico/?ref=3527",
       imageUrl: "https://raw.githubusercontent.com/andressadesign/files/main/images-produtos/necessaire-gato.png"
     },
