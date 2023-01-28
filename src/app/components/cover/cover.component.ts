b58a221bc774357fde6a5bf7fa8643fcc94dd0cb
Diogo <diogozang2@gmail.com>
2023-01-28 11:17:33 -0300

    feat: basic new header implemented

--- src/app/components/cover/cover.component.ts
+++ src/app/components/cover/cover.component.ts
@@ -1,15 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-cover',
   templateUrl: './cover.component.html',
   styleUrls: ['./cover.component.scss']
 })
-export class CoverComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-}
+export class CoverComponent {}
